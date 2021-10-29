126889e428f9b82cbf97d72eb39954ccc807755f
Jay Rabjohns <jay@rabjohns.org>
2021-10-30 00:35:30 +0100

    Working backup of nextcloud

--- nextcloud-backup/nextcloud_backup.sh
+++ nextcloud-backup/nextcloud_backup.sh
@@ -3,35 +3,35 @@
 # Backup solution based on https://kevq.uk/how-to-backup-nextcloud/
 # Argument passing based on https://tecadmin.net/pass-command-line-arguments-in-shell-script/
 
-CurrentDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
-DataSourceDir=""    # /mnt/cloud/data/
-BackupParentDir=""  # /mnt/backups/nextcloud/
-LogsDir=""
-Verbose="false"
+currentDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
+dataSourceDir=""    # /mnt/cloud/data/
+backupParentDir=""  # /mnt/backups/nextcloud/
+logsDir=""
+verbose=false
 
-PrintUsage() 
+printUsage() 
 {
-    printf 
-    "-h --help      shows this help message" +
-    "-s --src       directory for data to backup\n" + 
-    "-d --dest      directory for backup storage\n" +
-    "-v --verbose   increases verbosity\n"
+    echo "  -h  shows this help message"
+    echo "  -s  directory for data to backup"
+    echo "  -d  directory for backup storage"
+    echo "  -v  increases verbosity"
 }
 
-Log()
-{
-    if [ "$Verbose" = "true" ]; then
-        echo "$1"
-    fi
+# log()
+# {
+#     if [ "$verbose" = true ]; then
+#         echo "$1"
+#         echo "Verbose!"
+#     fi
 
-    echo "$1" >> "$LogsDir""$CurrentDateTime"
-}
+#     echo "$1" >> "$logsDir""$currentDateTime.log"
+# }
 
-RunBackup()
+runBackup()
 {
-    dataDir="$BackupParentDir""data/"
-    databaseDir="$BackupParentDir""database/"
-    oldDataDestinationDir="$BackupParentDir""old/"
+    dataDir="$backupParentDir""data/"
+    databaseDir="$backupParentDir""database/"
+    oldDataDestinationDir="$backupParentDir""old/"
 
     # Creating directories if needed
     if [ ! -d "$dataDir" ]; then
@@ -45,90 +45,93 @@ RunBackup()
     fi
 
     # Creating backup metadata file
-    echo "date: ${CurrentDateTime}" > "$BackupParentDir""backup_metadata"
-
-    # Output to a logfile
-    # exec &> "$LogsDir$CurrentDateTime.txt"
+    echo "date: ${currentDateTime}" > "$backupParentDir""backup_metadata"
 
     # Compressing old backup, if data exists
-    if find -- "$dataDir" -prune -type d -empty | grep -q '^'; then
-    Log "$dataDir is empty, skipping compression of old backup";
+    if [ -z "$(ls -A $dataDir)" ]; then
+        echo "$dataDir is empty, skipping compression of old backup";
     else
-    Log "Compressing old backup..."
-    tar -zcf "${oldDataDestinationDir}${CurrentDateTime}.tar.gz" "${dataDir}" "${databaseDir}" "${LogsDir}"
+        echo "--Compressing old backup...--"
+        tar -zcf "${oldDataDestinationDir}${currentDateTime}.tar.gz" "${dataDir}" "${databaseDir}" "${logsDir}"
     fi
 
     # Backup data
-    Log "Starting data backup..."
+    echo "--Starting data backup...--"
     nextcloud.occ maintenance:mode --on
-    rsync -Aavx "${DataSourceDir}" "${dataDir}"
+    rsync -Aavx "${dataSourceDir}" "${dataDir}"
     nextcloud.occ maintenance:mode --off
-    Log "Data backup finished"
+    echo "--Data backup finished--"
 
     # Export apps, database, config
-    Log "Starting export of apps, database, and config..." 
+    echo "--Starting export of apps, database, and config...--" 
     nextcloud.export -abc
-    Log "Export complete"
+    echo "--Completed export of apps, database, and config--"
 
     # Compress export
-    Log "Compressing export..."
-    tar -zcf "${databaseDir}${CurrentDateTime}.tar.gz" /var/snap/nextcloud/common/backups/*
-    Log "Export successfully compressed to ${databaseDir}"
+    echo "--Compressing export...--"
+    tar -zcf "${databaseDir}${currentDateTime}.tar.gz" /var/snap/nextcloud/common/backups/*
+    echo "--Export compressed to ${databaseDir}--"
 
     # Remove uncompressed export
     rm -rf /var/snap/nextcloud/common/backups/*
 
     # Remove old backups older than 14 days
-    Log "Removing backups older than 14 days..."
+    echo "--Removing backups older than 14 days...--"
     find "${oldDataDestinationDir}" -mtime +14 -type f -delete
-    Log "Old backups succefully removed"
+    echo "--Old backups removed--"
 
-    Log "Nextcloud backup completed successfully."
+    echo "--Nextcloud backup completed successfully.--"
 }
 
 
 ######      Main      ######
-ARGS=$(getopt -a --options s:d:vh --long "src:,dest:,verbose,help" -- "$@")
-eval set -- "$ARGS"
+
+# Check if user is sudo or root
+if [ "$(whoami)" != root ]; then
+    echo "You need to be root or use sudo to run this."
+    exit;
+fi
 
 # Parameter selection
-while true; do
-    case "$1" in
-        -s|--src)
-        DataSourceDir="$2"
-        shift 2;;
-        -d|--dest)
-        BackupParentDir="$2"
-        shift 2;;
-        -v|--verbose)
-        Verbose="true"
-        shift 2;;
-        -h|--help)
-        PrintUsage
-        shift 2;;
-        --)
-        break;;
+while getopts ":hvs:d:" option; do
+    case $option in
+        h) # Display help
+            printUsage
+            exit;;
+        v) # Increases verbosity
+            verbose=true;;
+        s) #Defines source directory
+            dataSourceDir="$OPTARG";;
+        d) #Defines destination directory
+            backupParentDir="$OPTARG";;
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
     esac
 done
 
 # Parameter validation
-if [ ! -d "$DataSourceDir" ]; then 
-    echo "Provide a valid source directory."
-elif [ "$BackupParentDir" = "" ]; then
-    echo "Provide a destination directory"
+if [[ ! -d "$dataSourceDir" || ! "$dataSourceDir" == */ ]]; then
+    echo "Provide a valid source directory. (Be sure to include a slash at the end)"
+    exit;
+elif [[ "$backupParentDir" = "" || ! "$backupParentDir" == */ ]]; then
+    echo "Provide a destination directory. (Be sure to include a slash at the end)"
+    exit;
+fi
+
+logsDir=$backupParentDir"logs/"
+
+# Creating necessary directories if needed
+if [ ! -d "$backupParentDir" ]; then
+    mkdir "$backupParentDir"
+fi
+if [ ! -d "$logsDir" ]; then
+    mkdir "$logsDir"
+fi
+
+# Printing to log file and or stdout
+if [ "$verbose" = true ]; then
+    runBackup | tee "$logsDir""$currentDateTime.log"
 else
-    LogsDir=$BackupParentDir"logs/"
-
-    # Creating necessary directories if needed
-    if [ ! -d "$BackupParentDir" ]; then
-        mkdir "$BackupParentDir"
-    fi
-    if [ ! -d "$LogsDir" ]; then
-        mkdir "$LogsDir"
-    fi
-
-    # Creating log file
-    touch "$LogsDir$CurrentDateTime.txt"
-
-    RunBackup
+    runBackup > "$logsDir""$currentDateTime.log"
 fi
