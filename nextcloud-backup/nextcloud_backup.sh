bb963c5763593814b0e3bed3b8e6e23fa05f8b36
jayrabjohns <jay@rabjohns.org>
2021-11-17 20:15:27 +0000

    Refactoring & new flag for keeping old backups

--- nextcloud-backup/nextcloud_backup.sh
+++ nextcloud-backup/nextcloud_backup.sh
@@ -3,27 +3,39 @@
 # Backup solution based on https://kevq.uk/how-to-backup-nextcloud/
 # Argument passing based on https://www.redhat.com/sysadmin/arguments-options-bash-scripts
 
-currentDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
-dataSourceDir=""    # /mnt/cloud/data/
-backupParentDir=""  # /mnt/backups/nextcloud/
-logsDir=""
-verbose=false
+CurrentDateTime=$(date +"%Y-%m-%d_%H-%M-%S")
+DataSourceDir=""
+BackupParentDir=""
+LogsDir=""
+RemoveBackupsOlderThan="14"
 
-printUsage() 
+IsVerbose=false
+KeepBackups=false
+
+PrintUsage()
 {
+    echo ""
+    echo "Flags:"
     echo "  -h  shows this help message"
+    echo "  -v  enables verbose mode"
+    echo "  -k  indicates to compress and copy current backups before overwriting them"
+    echo ""
+    echo "Parameters:"
+    echo "  -s  <file path> specifies nextcloud data source directory"
+    echo "  -d  <file path> specifies destination directory for backup"
     echo "  -s  directory for data to backup"
     echo "  -d  directory for backup storage"
     echo "  -v  increases verbosity"
+    echo ""
 }
 
-runBackup()
+RunBackup()
 {
     SECONDS=0
-    dataDir="${backupParentDir}data/"
-    databaseDir="${backupParentDir}database/"
-    oldDataDestinationDir="${backupParentDir}old/"
-    metadataFile="${backupParentDir}backup_metadata"
+    dataDir="${BackupParentDir}data/"
+    databaseDir="${BackupParentDir}database/"
+    oldBackupsDir="${BackupParentDir}old/"
+    metadataFile="${BackupParentDir}backup_metadata"
 
     # Creating directories if needed
     if [ ! -d "$dataDir" ]; then
@@ -32,12 +44,14 @@ runBackup()
     if [ ! -d "$databaseDir" ]; then
         mkdir "$databaseDir"
     fi
-    if [ ! -d "$oldDataDestinationDir" ]; then
-        mkdir "$oldDataDestinationDir"
+    if [ ! -d "$oldBackupsDir" ]; then
+        mkdir "$oldBackupsDir"
     fi
 
+    echo ""
     echo "Starting nextcloud backup..."
 
+    if [ "$KeepBackups" = true ]; then
     # Compressing old backup, if data exists
     echo ""
     echo "--Compressing old backup...--"
@@ -47,21 +61,22 @@ runBackup()
         start="$SECONDS"
         oldDate=$(grep -oP  "(?<=^date: ).*$" "$metadataFile")
         #echo "$oldDate"
-        tar -zcf "${oldDataDestinationDir}${oldDate}.tar.gz" "$dataDir" "${databaseDir}db_export_${oldDate}.tar.gz" "${logsDir}${oldDate}.log"
+            tar -zcf "${oldBackupsDir}${oldDate}.tar.gz" "$dataDir" "${databaseDir}db_export_${oldDate}.tar.gz" "${LogsDir}${oldDate}.log"
 
         duration=$((SECONDS - start))
-        echo "--Old backup compressed and moved to $oldDataDestinationDir in $((duration / 60)) minutes and $((duration % 60)) seconds--"
+            echo "--Old backup compressed and moved to $oldBackupsDir in $((duration / 60)) minutes and $((duration % 60)) seconds--"
+        fi
     fi
 
     # Creating new metadata file
-    echo "date: $currentDateTime" > "${backupParentDir}backup_metadata"
+    echo "date: $CurrentDateTime" > "${BackupParentDir}backup_metadata"
 
     # Backup data
     echo ""
     echo "--Starting data backup...--"
     start=$SECONDS
     nextcloud.occ maintenance:mode --on
-    rsync -Aavx "$dataSourceDir" "$dataDir"
+    rsync -Aavx "$DataSourceDir" "$dataDir"
     nextcloud.occ maintenance:mode --off
     duration=$((SECONDS - start))
     echo "--Data backup finished in $((duration / 60)) minutes and $((duration % 60)) seconds--"
@@ -75,19 +90,19 @@ runBackup()
     # Compress export
     echo ""
     echo "--Compressing export...--"
-    tar -zcf "${databaseDir}db_export_${currentDateTime}.tar.gz" /var/snap/nextcloud/common/backups/*
+    tar -zcf "${databaseDir}db_export_${CurrentDateTime}.tar.gz" /var/snap/nextcloud/common/backups/*
     duration=$((SECONDS - start))
     echo "--Exported compressed apps, database, and config to $databaseDir in $((duration / 60)) minutes and $((duration % 60)) seconds--"
 
     # Remove uncompressed export
+    echo ""
     echo "--Removing uncompressed exports...--"
     start=$SECONDS
     rm -rf /var/snap/nextcloud/common/backups/*
     duration=$((SECONDS - start))
     echo "--Finished removing uncompressed exports in $((duration / 60)) minutes and $((duration % 60)) seconds"
 
-    # Remove old backups older than 14 days
-    echo "--Removing backups, logs, & database exports older than 14 days...--"
+    echo ""
     start=$SECONDS
     find "$oldDataDestinationDir" -mtime +14 -type f -delete
     find "$databaseDir" -mtime +14 -type f -delete
@@ -96,6 +111,7 @@ runBackup()
     echo "--Old backups, logs, & database exports removed in $((duration / 60)) minutes and $((duration % 60)) seconds--"
 
     duration=$SECONDS
+    echo ""
     echo "--Nextcloud backup completed successfully in $((duration / 60)) minutes and $((duration % 60)) seconds--"
 }
 
@@ -105,49 +121,56 @@ runBackup()
 # Check if user is sudo or root
 if [ "$(whoami)" != root ]; then
     echo "You need to be root or use sudo to run this."
-    exit;
+    exit 1;
 fi
 
 # Parameter selection
-while getopts ":hvs:d:" option; do
+while getopts ":hvks:d:r:" option; do
     case $option in
         h) # Display help
-            printUsage
-            exit;;
-        v) # Increases verbosity
-            verbose=true;;
-        s) # Defines source directory
-            dataSourceDir="$OPTARG";;
-        d) # Defines destination directory
-            backupParentDir="$OPTARG";;
+            PrintUsage
+            exit 0;;
+        v) # Enable verbose mode
+            IsVerbose=true;;
+        k) # Indicates we should compress & copy old backups before overriding them
+            KeepBackups=true;;
+        s) # Source directory
+            DataSourceDir="$OPTARG";;
+        d) # Destination directory
+            BackupParentDir="$OPTARG";;
+        r) # Days before old backups are deleted
+            RemoveBackupsOlderThan="$OPTARG";;
         \?) # Invalid option
-            echo "Error: Invalid option"
-            exit;;
+            echo ""
+            echo "Error: Invalid option."
+            echo "Try usingse '-h' for more information."
+            echo ""
+            exit 1;;
     esac
 done
 
 # Parameter validation
-if [[ ! -d "$dataSourceDir" || ! "$dataSourceDir" == */ ]]; then
+if [[ ! -d "$DataSourceDir" || ! "$DataSourceDir" == */ ]]; then
     echo "Provide a valid source directory. (Be sure to include a slash at the end)"
-    exit;
-elif [[ "$backupParentDir" = "" || ! "$backupParentDir" == */ ]]; then
+    exit 1;
+elif [[ "$BackupParentDir" = "" || ! "$BackupParentDir" == */ ]]; then
     echo "Provide a valid destination directory. (Be sure to include a slash at the end)"
-    exit;
+    exit 1;
 fi
 
-logsDir="${backupParentDir}logs/"
+LogsDir="${BackupParentDir}logs/"
 
 # Creating necessary directories if needed
-if [ ! -d "$backupParentDir" ]; then
-    mkdir "$backupParentDir"
+if [ ! -d "$BackupParentDir" ]; then
+    mkdir "$BackupParentDir"
 fi
-if [ ! -d "$logsDir" ]; then
-    mkdir "$logsDir"
+if [ ! -d "$LogsDir" ]; then
+    mkdir "$LogsDir"
 fi
 
 # Printing to log file and or stdout
-if [ "$verbose" = true ]; then
-    runBackup | tee "${logsDir}${currentDateTime}.log"
+if [ "$IsVerbose" = true ]; then
+    RunBackup | tee "${LogsDir}${CurrentDateTime}.log"
 else
-    runBackup > "${logsDir}${currentDateTime}.log"
+    RunBackup > "${LogsDir}${CurrentDateTime}.log"
 fi
